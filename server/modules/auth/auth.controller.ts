2fbd4731443c0de9166b4289cad10e349dbc3c11
huazai128 <zhouweihua@joyy.com>
2022-03-24 19:29:36 +0800

    feat: 接口获取sdk🚀

--- server/modules/auth/auth.controller.ts
+++ server/modules/auth/auth.controller.ts
@@ -1,14 +1,9 @@
-import { HttpRequest } from "@app/interfaces/request.interface";
-import { TransformPipe } from "@app/pipes/transform.pipe";
-import { Body, Controller, Get, Param, Post, Req, Res  } from "@nestjs/common";
+import {  Controller, Get, Param  } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { Request, Response } from 'express'
-import { ResponseStatus } from "@app/interfaces/response.interface";
 import { Responsor } from "@app/decorators/responsor.decorator";
 
 @Controller('api')
 export class AuthController {
-
     constructor(private readonly authService: AuthService) { }
     /**
      *
--- server/modules/wechat/wechat.controller.ts
+++ server/modules/wechat/wechat.controller.ts
@@ -1,7 +1,7 @@
 import { ScopeEnum } from "@app/constants/text.constant";
 import { QueryParams } from "@app/decorators/params.decorator";
-import { Controller, Get, Res } from "@nestjs/common";
-import { Response } from "express";
+import { Controller, Get, Res, Req } from "@nestjs/common";
+import { Response, Request } from "express";
 import { WechatService } from "./wechat.service";
 
 @Controller('wx')
@@ -9,12 +9,18 @@ export class WechatController {
     constructor(
         private readonly wechatService: WechatService
     ) {
-        
+
     }
 
     @Get('toAuth')
     getAuth(@QueryParams('query') query: any,@Res() res:Response) {
         const url = this.wechatService.getAuthorizeUrl(query.redirectUrl,ScopeEnum.SNSAPI_USERINFO, '12121');
         return res.status(301).redirect(url)
+    }
+
+    @Get('jssdk')
+    async getJsSdk (@Req() req: Request) {
+        const url =  req.get('referer') as string
+        return await this.wechatService.getSdk(url);
     }
 }
