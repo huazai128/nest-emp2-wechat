3fb0f09b3738970c463a74fbd180e588adb33315
huazai128 <zhouweihua@joyy.com>
2022-06-13 10:31:20 +0800

    feat: 默认上报行为长度

--- src/utils/monitor/index.ts
+++ src/utils/monitor/index.ts
@@ -11,6 +11,7 @@ import SendLog from "./send";
 interface MonitorProps {
     url: string // 上报Url
     isExposure?: boolean // 是否支持曝光埋点
+    behaviorLen?: number // 上报行为的长度，去数组最后值，为负数。
 }
 export default class Monitor {
     private webVitals: WebVitals
@@ -19,13 +20,13 @@ export default class Monitor {
     private send: SendLog
     public customHandler: FN1
     public initReactError: FN2<Error, ErrorInfo>
-    constructor({ url, isExposure }: MonitorProps) {
+    constructor({ url, isExposure, behaviorLen }: MonitorProps) {
         this.send = new SendLog(url)
         this.webVitals = new WebVitals({ ...this.send })
         this.userVitals = new UserVitals({ ...this.send, isExposure: !!isExposure })
         this.errorVitals = new ErrorVitals({
             behaviorTracking: this.userVitals.behaviorTracking,
-            behaviorLen: -30,
+            behaviorLen: behaviorLen,
             ...this.send
         })
         this.customHandler = this.userVitals.initCustomerHandler()
